269ff522f836eca3aab7a2ba9261bdf9d49d632c
zhangr648 <bubu.ok@outlook.com>
2021-04-01 20:51:31 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,223 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
+// detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '../views/layout/Layout'
+
+/**
+* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+*                                if not set alwaysShow, only more than one route under the children
+*                                it will becomes nested mode, otherwise not show the root menu
+* redirect: noredirect           if `redirect:noredirect` will no redirect in the breadcrumb
+* name:'router-name'             the name is used by <keep-alive> (must set!!!)
+* meta : {
+    title: 'title'               the name show in submenu and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar,
+  }
+**/
+export const constantRouterMap = [
+  { path: '/login', component: () => import('@/views/login/index'), hidden: true },
+  { path: '/404', component: () => import('@/views/404'), hidden: true },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    name: 'Dashboard',
+    hidden: true,
+    children: [{
+      path: 'dashboard',
+      component: () => import('@/views/dashboard/index')
+    }]
+  },
+
+  {
+    path: '/edu/teacher',
+    component: Layout,
+    redirect: '/edu/teacher/table',
+    name: '学生管理',//讲师管理
+    meta: { title: '学生管理', icon: 'example' },//讲师管理
+    children: [
+      {
+        path: 'table',
+        name: '学生列表',//讲师列表
+        component: () => import('@/views/edu/teacher/list'),
+        meta: { title: '学生列表', icon: 'table' }//讲师列表
+      },
+      {
+        path: 'save',
+        name: '添加学生',  //添加讲师
+        component: () => import('@/views/edu/teacher/save'),
+        meta: { title: '添加学生', icon: 'tree' }//添加讲师
+      },
+      {
+        path: 'edit/:id',   
+        name: 'EduTeacherEdit',
+        component: () => import('@/views/edu/teacher/save'),
+        meta: { title: '编辑学生', noCache: true },//编辑讲师
+        hidden: true
+      }
+    ]
+  },
+
+  {
+    path: '/edu/subject',
+    component: Layout,
+    redirect: '/edu/subject/list',
+    name: '传感器管理',//课程分类管理
+    meta: { title: '传感器分类管理', icon: 'example' },//课程分类管理
+    children: [
+      {
+        path: 'list',
+        name: '传感器分类查看',//课程分类列表
+        component: () => import('@/views/edu/subject/list'),
+        meta: { title: '传感器分类查看', icon: 'table' }//课程分类列表
+      },
+      {
+        path: 'save',
+        name: '添加传感器节点',  //添加课程分类
+        component: () => import('@/views/edu/subject/save'),
+        meta: { title: '添加传感器节点', icon: 'tree' }//添加课程分类
+      }
+    ]
+  },
+
+  {
+    path: '/edu/course',
+    component: Layout,
+    redirect: '/course/list',
+    name: '设备管理',//课程管理
+    meta: { title: '传感器管理', icon: 'example' },//课程管理
+    children: [
+      {
+        path: 'list',
+        name: '设备列表',//课程列表
+        component: () => import('@/views/edu/course/list'),
+        meta: { title: '设备列表', icon: 'table' }//课程列表
+      },
+      {
+        path: 'info',
+        name: '添加设备',  //添加课程
+        component: () => import('@/views/edu/course/info'),
+        meta: { title: '添加设备', icon: 'tree' }//添加课程
+      },
+      {
+        path: 'info/:id',
+        name: 'EduCourseInfoEdit',
+        component: () => import('@/views/edu/course/info'),
+        meta: { title: '编辑设备基本信息', noCache: true },//编辑课程基本信息
+        hidden: true
+      },
+      {
+        path: 'chapter/:id',
+        name: 'EduCourseChapterEdit',
+        component: () => import('@/views/edu/course/chapter'),
+        meta: { title: '编辑设备位置信息', noCache: true },//编辑课程大纲
+        hidden: true
+      },
+      {
+        path: 'publish/:id',
+        name: 'EduCoursePublishEdit',
+        component: () => import('@/views/edu/course/publish'),
+        meta: { title: '确定设备报修', noCache: true },//发布课程
+        hidden: true
+      }
+    ]
+  },
+
+  {
+    path: '/form',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Form',
+        component: () => import('@/views/form/index'),
+        meta: { title: 'Form', icon: 'form' }
+      }
+    ]
+  },
+
+  {
+    path: '/nested',
+    component: Layout,
+    redirect: '/nested/menu1',
+    name: 'Nested',
+    meta: {
+      title: 'Nested',
+      icon: 'nested'
+    },
+    children: [
+      {
+        path: 'menu1',
+        component: () => import('@/views/nested/menu1/index'), // Parent router-view
+        name: 'Menu1',
+        meta: { title: 'Menu1' },
+        children: [
+          {
+            path: 'menu1-1',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'Menu1-1',
+            meta: { title: 'Menu1-1' }
+          },
+          {
+            path: 'menu1-2',
+            component: () => import('@/views/nested/menu1/menu1-2'),
+            name: 'Menu1-2',
+            meta: { title: 'Menu1-2' },
+            children: [
+              {
+                path: 'menu1-2-1',
+                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+                name: 'Menu1-2-1',
+                meta: { title: 'Menu1-2-1' }
+              },
+              {
+                path: 'menu1-2-2',
+                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+                name: 'Menu1-2-2',
+                meta: { title: 'Menu1-2-2' }
+              }
+            ]
+          },
+          {
+            path: 'menu1-3',
+            component: () => import('@/views/nested/menu1/menu1-3'),
+            name: 'Menu1-3',
+            meta: { title: 'Menu1-3' }
+          }
+        ]
+      },
+      {
+        path: 'menu2',
+        component: () => import('@/views/nested/menu2/index'),
+        meta: { title: 'menu2' }
+      }
+    ]
+  },
+
+  {
+    path: 'external-link',
+    component: Layout,
+    children: [
+      {
+        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+        meta: { title: 'External Link', icon: 'link' }
+      }
+    ]
+  },
+
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+export default new Router({
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
